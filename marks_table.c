9f1cc3a99a86f7d02690e83f01e24d77fcdc0654
Arya Sharma <aryasharma9125as@gmail.com>
2026-03-07 18:55:35 +0530

    Create marks_table.c

---
+++ marks_table.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+printf("subject             marks\n\n"
+       "mathematics         90\n"
+       "computer            77\n"
+       "chemistry           69");
+getch();
+return 0;
+}

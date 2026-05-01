aced85c61befeb77448854fa34157926e97c407f
Arya Sharma <aryasharma9125as@gmail.com>
2026-05-01 20:34:50 +0530

    Create odd_or_even.c

---
+++ Operators_&_Expressions/odd_or_even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter any integer:");
+scanf("%d",&num);
+if (num%2==0){
+printf("%d is an even number.\n",num);
+}
+else {
+printf("%d is an odd number .\n",num);
+}
+getch();
+}
